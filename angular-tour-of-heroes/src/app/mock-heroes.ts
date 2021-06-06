de25582d0b5097c563b8cc23adcafa38db4d1e1f
Lukasz Cwiklinski <lukasz.cwiklinski@stxnext.pl>
2021-06-06 14:20:21 +0200

    tour of heroes

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Dr Nice'},
+  {id: 12, name: 'Narco'},
+  {id: 13, name: 'Bombasto'},
+  {id: 14, name: 'Supraman'},
+  {id: 15, name: 'Baitman'},
+  {id: 16, name: 'Junker'},
+  {id: 17, name: 'Mr IQ'},
+  {id: 18, name: 'Songo'},
+  {id: 19, name: '2 punch face'},
+  {id: 20, name: 'Godzilla'},
+];
